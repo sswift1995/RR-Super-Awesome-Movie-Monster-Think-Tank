dd34e6c6aa889173e7804fc6634616a1821ea35e
Syrinthia Swift <116780275+sswift1995@users.noreply.github.com>
2023-04-24 08:11:19 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Loader from 'react-loader-spinner';
+import react, { Suspense } from 'react';
+import AboutUs from './components/AboutUs';
+import Packages from './components/Packages';
+
 import React from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
 
 function App() {
   return (
@@ -9,11 +12,20 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+      <Suspense fallback={<Loader 
+            type="Bars" 
+            color="#FF00FF" 
+            height={200} 
+            width={200} 
+            style={{margin: "0 auto"}}/>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
